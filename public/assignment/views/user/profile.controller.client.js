89e479c802b50c38590365d5a3f50431076cac54
PriyaArun <lakprigan@gmail.com>
2016-05-25 10:19:26 -0400

    added update user feature

--- public/assignment/views/user/profile.controller.client.js
+++ public/assignment/views/user/profile.controller.client.js
@@ -6,20 +6,32 @@
         .module("WebAppMaker")
         .controller("ProfileController", ProfileController);
 
+    var Users = [
+        {_id: "123", username: "alice",    password: "alice",    firstName: "Alice",  lastName: "Wonder"  },
+        {_id: "234", username: "bob",      password: "bob",      firstName: "Bob",    lastName: "Marley"  },
+        {_id: "345", username: "charly",   password: "charly",   firstName: "Charly", lastName: "Garcia"  },
+        {_id: "456", username: "jannunzi", password: "jannunzi", firstName: "Jose",   lastName: "Annunzi" }
+    ];
+
     function ProfileController($routeParams) {
-        var Users = [
-            {_id: "123", username: "alice",    password: "alice",    firstName: "Alice",  lastName: "Wonder"  },
-            {_id: "234", username: "bob",      password: "bob",      firstName: "Bob",    lastName: "Marley"  },
-            {_id: "345", username: "charly",   password: "charly",   firstName: "Charly", lastName: "Garcia"  },
-            {_id: "456", username: "jannunzi", password: "jannunzi", firstName: "Jose",   lastName: "Annunzi" }
-        ]
+
         var ViewModel = this;
+        var index = -1;
+        ViewModel.UpdateUser = UpdateUser;
+
         var id = $routeParams.id;
         for (var key in Users){
-            if(Users[key]._id===id){
+            if(Users[key]._id==id){
                 ViewModel.User = Users[key];
+                index = key;
             }
         }
 
+        function UpdateUser(newUser) {
+            if(index!= -1){
+            Users[index].firstName = newUser.firstName;
+            Users[index].lastName = newUser.lastName;
+        }
+        }
     }
 })();
