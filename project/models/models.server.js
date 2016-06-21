67ba768c6c082e6fadf28e2170a9cb71e59d7ad5
PriyaArun <lakprigan@gmail.com>
2016-06-21 13:05:36 -0400

    added portfolio service modified circle modified portfolio client Added portfolio model included portfolio model added portfolio server service included portfolio server service in app added portfolio client service added portfolio edit view added portfolio new added new models

--- project/models/models.server.js
+++ project/models/models.server.js
@@ -5,7 +5,11 @@ module.exports = function () {
     
     var models = {
         UserModel: require("./user/user.model.server") (),
-        PortfolioModel: require("./portfolio/portfolio.model.server") ()
+        PortfolioModel: require("./portfolio/portfolio.model.server") (),
+        StockModel: require("./stock/stock.model.server") (),
+        CommentModel: require("./comment/comment.model.server") (),
+        StockDataModel: require("./stockdata/stockdata.model.server") ()
+
     }
     return models;
 };
