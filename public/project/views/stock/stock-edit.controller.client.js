2436e497299e21d52a8ba95701c0b83214a330ca
PriyaArun <lakprigan@gmail.com>
2016-06-21 12:52:20 -0400

    added portfolio service modified circle modified portfolio client Added portfolio model included portfolio model added portfolio server service included portfolio server service in app added portfolio client service added portfolio edit view added portfolio new added stock edit controller

---
+++ public/project/views/stock/stock-edit.controller.client.js
@@ -0,0 +1,38 @@
+/**
+ * Created by PriyaArun on 5/29/16.
+ */
+(function () {
+    angular
+        .module("StockWatch")
+        .controller("StockEditController", StockEditController);
+
+    function StockEditController($location, $routeParams, StockService) {
+        var ViewModel = this;
+
+        Initialize();
+
+        function Initialize() {
+            ViewModel.UserId = $routeParams.id;
+            ViewModel.PortfolioId = $routeParams.pid;
+            ViewModel.StockId= $routeParams.sid;
+
+            ViewModel.UpdateStock = UpdateStock;
+            StockService
+                .FindStockById(ViewModel.StockId)
+                .then(function (res) {
+                    ViewModel.Stock = res.data;
+                })
+        }
+
+        function UpdateStock() {
+            StockService
+                .UpdateStock(ViewModel.StockId, ViewModel.Stock)
+                .then(function (res) {
+                    $location.url("/user/"+ViewModel.UserId+"/portfolio/"+ViewModel.PortfolioId+"/stock");
+                },function (err) {
+                    ViewModel.error = "Unable to update the Stock";
+                });
+        }
+    }
+})();
+
