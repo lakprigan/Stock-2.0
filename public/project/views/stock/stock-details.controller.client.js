1eba7342dcd9c514539e6e4bd8ed31935aa78acb
PriyaArun <lakprigan@gmail.com>
2016-06-21 12:52:41 -0400

    added portfolio service modified circle modified portfolio client Added portfolio model included portfolio model added portfolio server service included portfolio server service in app added portfolio client service added portfolio edit view added portfolio new added stock-details controller

---
+++ public/project/views/stock/stock-details.controller.client.js
@@ -0,0 +1,53 @@
+/**
+ * Created by PriyaArun on 6/1/16.
+ */
+(function () {
+    angular
+        .module("StockWatch")
+        .controller("StockDetailsController", StockDetailsController);
+
+    function StockDetailsController($routeParams, QuandlService, StockService) {
+        var vm = this;
+        vm.UserId = $routeParams.id;
+        vm.PortfolioId = $routeParams.pid;
+        vm.StockId= $routeParams.sid;
+        StockService.FindStockById(vm.StockId)
+            .then(function (res) {
+                vm.Stock = res.data;
+                QuandlService.getStockData(vm.Stock.code)
+                    .then(function (response) {
+                        var data = [];
+                        vm.indexValue = response.data.dataset.data[0][1];
+                        vm.high = response.data.dataset.data[0][2];
+                        vm.low = response.data.dataset.data[0][3];
+                        vm.totalMarketValue = response.data.dataset.data[0][4];
+                        _.forEach(response.data.dataset.data, function (day) {
+                            data.push([
+                                new Date(day[0]).getTime(),
+                                day[1]
+                            ]);
+                        })
+                        _.reverse(data);
+                        $('#chartContainer').highcharts('StockChart', {
+                            rangeSelector: {
+                                selected: 1
+                            },
+                            title: {
+                                text: vm.Stock.code
+                            },
+                            series: [{
+                                name: vm.Stock.code,
+                                data: data,
+                                tooltip: {
+                                    valueDecimals: 2
+                                }
+                            }]
+                        });
+                    }, function (err) {
+                        vm.Error = err.data;
+                    });
+            },function (err) {
+                vm.Error = err.data;
+            });
+    }
+})();
