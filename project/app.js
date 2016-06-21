c5adc083a531f04bf207b9213aafd5abf5b4e072
PriyaArun <lakprigan@gmail.com>
2016-06-21 12:48:17 -0400

    added portfolio service modified circle modified portfolio client Added portfolio model included portfolio model added portfolio server service included portfolio server service in app added portfolio client service added portfolio edit view added portfolio new added route for stock data

--- project/app.js
+++ project/app.js
@@ -5,4 +5,7 @@ module.exports= function (app){
     var models = require("./models/models.server.js")();
     require("./services/user.service.server.js")(app, models);
     require("./services/portfolio.service.server.js")(app, models);
+    require("./services/stock.service.server.js")(app, models);
+    require("./services/comment.service.server.js")(app, models);
+    require("./services/stockdata.service.server.js")(app, models);
 };
