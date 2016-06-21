b2c030a0d65a3de45b9b1dfbbdec428cb9d9ee5c
PriyaArun <lakprigan@gmail.com>
2016-06-21 13:04:36 -0400

    added portfolio service modified circle modified portfolio client Added portfolio model included portfolio model added portfolio server service included portfolio server service in app added portfolio client service added portfolio edit view added portfolio new modified comment schema

--- project/models/comment/comment.schema.server.js
+++ project/models/comment/comment.schema.server.js
@@ -4,6 +4,7 @@
 module.exports=function () {
     var mongoose = require("mongoose");
     var CommentSchema = mongoose.Schema({
+        code: String,
         username : String,
         post : String,
         dateCreated : {type: Date, default: Date.Now}
