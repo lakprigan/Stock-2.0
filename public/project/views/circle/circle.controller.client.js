b9d30c6bde53e363a79d62d16e955314f65a28e2
PriyaArun <lakprigan@gmail.com>
2016-06-21 12:48:52 -0400

    added portfolio service modified circle modified portfolio client Added portfolio model included portfolio model added portfolio server service included portfolio server service in app added portfolio client service added portfolio edit view added portfolio new added circle controller

--- public/project/views/circle/circle.controller.client.js
+++ public/project/views/circle/circle.controller.client.js
@@ -1,9 +1,7 @@
 /**
  * Created by PriyaArun on 6/18/16.
  */
-/**
- * Created by PriyaArun on 5/24/16.
- */
+
 (function () {
     angular
         .module("StockWatch")
@@ -27,16 +25,20 @@
                 });
 
             ViewModel.UpdateUser = UpdateUser;
-            ViewModel.ToggleFollow = ToggleFollow;
-            
+            ViewModel.FollowUser = FollowUser;
+            ViewModel.UnfollowUser = UnfollowUser;
+            ViewModel.AvailableExperts = [];
             UserService
                 .GetExperts()
                 .then(function (res) {
                         ViewModel.AvailableExperts = res.data;
-                    angular.forEach(ViewModel.AvailableExperts, function (all) {
-                            angular.forEach(ViewModel.User.circle, function (follow, index) {
-                                if (all.username === follow) {
-                                    ViewModel.AvailableExperts.splice(all, 1);
+                        angular.forEach(ViewModel.AvailableExperts, function (value, key) {
+                            if(value.username === ViewModel.currentUser.username){
+                                ViewModel.AvailableExperts.splice(key, 1);
+                            }
+                            angular.forEach(ViewModel.User.circle, function (follow) {
+                                if (value.username == follow) {
+                                    ViewModel.AvailableExperts.splice(key, 1);
                                 }
                             })
                         });
@@ -46,17 +48,28 @@
                     });
         }
 
-        function ToggleFollow(user, action){
-            if(action === 'follow')
-                ViewModel.User.circle.push(user);
-            else {
-                angular.forEach(ViewModel.User.circle, function (name) {
-                    if(name === user && name!=null)
-                        ViewModel.User.circle.splice(name, 1);
+        function FollowUser(index) {
+            var user = ViewModel.AvailableExperts[index];
+            ViewModel.User.circle.push(user.username);
+            ViewModel.AvailableExperts.splice(index, 1);
+            UserService.UpdateUser(ViewModel.User._id,ViewModel.User)
+            .then(function (res) {
+
+            },function (err) {
+                ViewModel.Error = "Cannot update user"
+            });
+        }
+
+        function UnfollowUser(index) {
+            var user = {username : ViewModel.User.circle[index]};
+            ViewModel.AvailableExperts.push(user);
+            ViewModel.User.circle.splice(index, 1);
+            UserService.UpdateUser(ViewModel.User._id,ViewModel.User)
+                .then(function (res) {
+
+                },function (err) {
+                    ViewModel.Error = "Cannot update user"
                 });
-            }
-            UpdateUser(ViewModel.User);
-            Initialize();
         }
 
         function UpdateUser(updatedUser) {
