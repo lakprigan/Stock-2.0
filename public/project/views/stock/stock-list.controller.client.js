2757960150705324ad2588e9343b597bb47a6783
PriyaArun <lakprigan@gmail.com>
2016-06-21 12:52:02 -0400

    added portfolio service modified circle modified portfolio client Added portfolio model included portfolio model added portfolio server service included portfolio server service in app added portfolio client service added portfolio edit view added portfolio new added stock list controller

---
+++ public/project/views/stock/stock-list.controller.client.js
@@ -0,0 +1,53 @@
+/**
+ * Created by PriyaArun on 6/19/16.
+ */
+(function () {
+    angular
+        .module("StockWatch")
+        .controller("StockListController", StockListController);
+
+    function StockListController($routeParams, StockService, QuandlService) {
+        var ViewModel = this;
+        Initialize();
+
+        function Initialize() {
+            ViewModel.UserId = $routeParams.id;
+            ViewModel.PortfolioId = $routeParams.pid;
+            ViewModel.DeleteStock = DeleteStock;
+            ViewModel.UpdatedStocks = [];
+            StockService
+                .FindStocksByPortfolioId(ViewModel.PortfolioId)
+                .then(function (res) {
+                    ViewModel.Stocks = res.data;
+                    UpdateCurrentStockValue();
+                    if (ViewModel.UpdatedStocks.length === 0) {
+                        ViewModel.Error = "Cannot get current stock worth!"
+                    }
+                });
+        }
+
+        function DeleteStock(id) {
+        StockService
+            .DeleteStock(id).then(function (res) {
+            Initialize();
+        }, function (err) {
+            ViewModel.Error = "unable to delete the stock!";
+        })
+        }
+        
+        function UpdateCurrentStockValue() {
+            var updatedStocks = [];
+            angular.forEach(ViewModel.Stocks, function (stock) {
+                QuandlService
+                    .getStockData(stock.code)
+                   .then(function (res) {
+                        var curStock = res.data;
+                        var curRate = parseFloat(curStock.dataset.data[0][4]) * parseFloat(stock.investmentQuantity);
+                        stock.presentValue =  curRate - parseFloat(stock.investmentPrice);
+                        ViewModel.UpdatedStocks.push(stock);
+                    });
+            });
+        }
+
+    }
+})();
