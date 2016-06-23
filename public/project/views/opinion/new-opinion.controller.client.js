6e5168a61e51920d76611b71ac2ee27174a15379
PriyaArun <lakprigan@gmail.com>
2016-06-22 22:05:01 -0400

    added portfolio service modified circle modified portfolio client Added portfolio model included portfolio model added portfolio server service included portfolio server service in app added portfolio client service added portfolio edit view added portfolio new added comment view and controller added new opinion

---
+++ public/project/views/opinion/new-opinion.controller.client.js
@@ -0,0 +1,41 @@
+/**
+ * Created by PriyaArun on 6/21/16.
+ */
+(function () {
+    angular
+        .module("StockWatch")
+        .controller("NewOpinionController", NewOpinionController);
+
+    function NewOpinionController($location, $routeParams, OpinionService, UserService) {
+        var ViewModel = this;
+        Initialize();
+        function Initialize() {
+            ViewModel.UserId = $routeParams.id;
+            ViewModel.SubmittedClass = "";
+            ViewModel.CreateOpinion = CreateOpinion;
+            UserService.FindUserById(ViewModel.UserId)
+                .then(function (res) {
+                    ViewModel.User = res.data;
+                },function (err) {
+                    ViewModel.Error = "Could not retrieve the user"
+                });
+        }
+
+        function CreateOpinion(opinion) {
+            opinion.user = ViewModel.User;
+            opinion.adviceDate = Date.now();
+            ViewModel.SubmittedClass = "submitted";
+            if(opinion.title){
+                OpinionService
+                    .CreateOpinion(ViewModel.UserId, opinion)
+                    .then(function (res) {
+                        $location.url("/user/"+ViewModel.UserId+"/opinion");
+                    },function (err) {
+                        ViewModel.Error = "Unable to create a new opinion";
+                    });
+            }
+            else{
+                ViewModel.Error="Please enter the highlighted fields"
+            }}
+    }
+})();
