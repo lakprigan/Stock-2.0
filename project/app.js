40c50333a486642305734c6f1f3644a806575bff
PriyaArun <lakprigan@gmail.com>
2016-06-22 21:57:21 -0400

    added portfolio service modified circle modified portfolio client Added portfolio model included portfolio model added portfolio server service included portfolio server service in app added portfolio client service added portfolio edit view added portfolio new added comment view and controller added models requires

--- project/app.js
+++ project/app.js
@@ -1,11 +1,12 @@
 /**
  * Created by PriyaArun on 6/9/16.
  */
-module.exports= function (app){
+module.exports = function (app) {
     var models = require("./models/models.server.js")();
     require("./services/user.service.server.js")(app, models);
     require("./services/portfolio.service.server.js")(app, models);
     require("./services/stock.service.server.js")(app, models);
     require("./services/comment.service.server.js")(app, models);
     require("./services/stockdata.service.server.js")(app, models);
+    require("./services/opinion.service.server")(app, models);
 };
