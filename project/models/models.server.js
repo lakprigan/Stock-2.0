d5d1ede63fb7ee2e81496dca97739fe764667f14
PriyaArun <lakprigan@gmail.com>
2016-06-22 21:57:43 -0400

    added portfolio service modified circle modified portfolio client Added portfolio model included portfolio model added portfolio server service included portfolio server service in app added portfolio client service added portfolio edit view added portfolio new added comment view and controller added models

--- project/models/models.server.js
+++ project/models/models.server.js
@@ -2,14 +2,14 @@
  * Created by PriyaArun on 6/10/16.
  */
 module.exports = function () {
-    
-    var models = {
-        UserModel: require("./user/user.model.server") (),
-        PortfolioModel: require("./portfolio/portfolio.model.server") (),
-        StockModel: require("./stock/stock.model.server") (),
-        CommentModel: require("./comment/comment.model.server") (),
-        StockDataModel: require("./stockdata/stockdata.model.server") ()
 
+    var models = {
+        UserModel: require("./user/user.model.server")(),
+        PortfolioModel: require("./portfolio/portfolio.model.server")(),
+        StockModel: require("./stock/stock.model.server")(),
+        CommentModel: require("./comment/comment.model.server")(),
+        StockDataModel: require("./stockdata/stockdata.model.server")(),
+        OpinionModel: require("./opinion/opinion.model.server")()
     }
     return models;
 };
