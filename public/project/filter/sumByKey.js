25f8e13e370b8b880172876bcdb21a87749dca53
PriyaArun <lakprigan@gmail.com>
2016-06-22 22:01:23 -0400

    added portfolio service modified circle modified portfolio client Added portfolio model included portfolio model added portfolio server service included portfolio server service in app added portfolio client service added portfolio edit view added portfolio new added comment view and controller added sum filter

---
+++ public/project/filter/sumByKey.js
@@ -0,0 +1,19 @@
+/**
+ * Created by PriyaArun on 6/22/16.
+ */
+(function () {
+angular.module('StockWatch')
+    .filter('sumByKey', function() {
+        return function(data, key) {
+            if (typeof(data) === 'undefined' || typeof(key) === 'undefined') {
+                return 0;
+            }
+
+            var sum = 0;
+            for (var i = data.length - 1; i >= 0; i--) {
+                sum += parseInt(data[i][key]);
+            }
+
+            return sum;
+        };
+    })})();
