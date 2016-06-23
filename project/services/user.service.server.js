b9bdc4140b711cfb5d2e0949f8616c68c496bd70
PriyaArun <lakprigan@gmail.com>
2016-06-23 07:39:02 -0400

    changed redirect uris

--- project/services/user.service.server.js
+++ project/services/user.service.server.js
@@ -15,10 +15,9 @@ module.exports = function(app, models){
     var facebookConfig = {
         clientID     : process.env.FACEBOOK_CLIENT_ID,
         clientSecret : process.env.FACEBOOK_CLIENT_SECRET,
-        callbackURL  : process.env.FACEBOOK_CALLBACK_URL
+        callbackURL  : process.env.FACEBOOK_SW_CALLBACK_URL
     };
-    //856441532318-j7unfefmdfl4lhbu2d31a1oo9j1nbmkn.apps.googleusercontent.com
-    // R64r4GcA1MKWmdwfRI5zlUK0
+
     var googleConfig = {
         clientID     : process.env.GOOGLE_CLIENT_ID,
         clientSecret : process.env.GOOGLE_CLIENT_SECRET,
