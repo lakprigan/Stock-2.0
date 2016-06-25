534e57a0b100300b7887b11ed94e325eedd0011b
PriyaArun <lakprigan@gmail.com>
2016-06-24 22:03:29 -0400

    added links and modified header

--- public/project/views/home/home.controller.client.js
+++ public/project/views/home/home.controller.client.js
@@ -6,9 +6,11 @@
         .module("StockWatch")
         .controller("HomeController", HomeController);
 
-    function HomeController(QuandlService) {
+    function HomeController(QuandlService, $rootScope) {
         var vm = this;
+        vm.currentUser = $rootScope.currentUser;
         vm.code = "GOOG";
+        vm.companyName = "Alphabet Inc (GOOG) Prices, Dividends, Splits and Trading Volume";
         vm.getStockData = getStockData;
         vm.selectedObject = selectedObject
         getStockData();
@@ -16,6 +18,7 @@
         function selectedObject(code) {
             if(code!=undefined) {
                 vm.code = code.originalObject.code.replace("WIKI/", "");
+                vm.companyName = code.originalObject.companyName;
                 getStockData();
             }
         }
@@ -24,10 +27,12 @@
             QuandlService.getStockData(vm.code)
                 .then(function (response) {
                     var data = [];
+                    var volData = [];
+                    vm.description = response.data.dataset.description;
                     vm.indexValue = response.data.dataset.data[0][1];
                     vm.high = response.data.dataset.data[0][2];
                     vm.low = response.data.dataset.data[0][3];
-                    vm.totalMarketValue = response.data.dataset.data[0][4];
+                    vm.totalMarketValue = "$"+response.data.dataset.data[0][5];
                     _.forEach(response.data.dataset.data, function (day) {
                         data.push([
                             new Date(day[0]).getTime(),
@@ -35,7 +40,7 @@
                         ]);
                     })
                     _.reverse(data);
-                    $('#chartContainer').highcharts('StockChart', {
+                    $('#chartContainer1').highcharts('StockChart', {
                         rangeSelector: {
                             selected: 1
                         },
@@ -50,6 +55,69 @@
                             }
                         }]
                     });
+data = [];
+                    _.forEach(response.data.dataset.data, function (day) {
+                        data.push([
+                            new Date(day[0]).getTime(),
+                            day[1],
+                            day[2],
+                            day[3],
+                            day[4]
+                        ]);
+                        volData.push([
+                            new Date(day[0]).getTime(),
+                            day[5]
+                        ])
+                    });
+
+                    _.reverse(data);
+                    _.reverse(volData);
+
+                    $('#chartContainer2').highcharts('StockChart', {
+                        rangeSelector: {
+                            selected: 1
+                        },
+                        title: {
+                            text: vm.code
+                        },
+                        yAxis: [{
+                            labels: {
+                                align: 'right',
+                                x: -3
+                            },
+                            title: {
+                                text: 'O H L C'
+                            },
+                            height: '60%',
+                            lineWidth: 2
+                        }, {
+                            labels: {
+                                align: 'right',
+                                x: -3
+                            },
+                            title: {
+                                text: 'Volume'
+                            },
+                            top: '65%',
+                            height: '35%',
+                            offset: 0,
+                            lineWidth: 2
+                        }],
+                        series: [{
+                            type: 'candlestick',
+                            name: vm.code,
+                            data: data,
+                            tooltip: {
+                                valueDecimals: 2
+                            }
+                        }, {
+                            type: 'column',
+                            name: 'Volume',
+                            data: volData,
+                            yAxis: 1
+                        }]
+                    });
+
                 }, function (err) {
                     console.log("error");
                 });
