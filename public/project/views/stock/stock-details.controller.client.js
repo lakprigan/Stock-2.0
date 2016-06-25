2ee6ad6d3c54881beced1ac8ac8ac1f78ad33190
PriyaArun <lakprigan@gmail.com>
2016-06-24 22:05:11 -0400

    added links and modified header

--- public/project/views/stock/stock-details.controller.client.js
+++ public/project/views/stock/stock-details.controller.client.js
@@ -14,13 +14,16 @@
         StockService.FindStockById(vm.StockId)
             .then(function (res) {
                 vm.Stock = res.data;
-                QuandlService.getStockData(vm.Stock.code)
+                vm.code = vm.Stock.code;
+                QuandlService.getStockData(vm.code)
                     .then(function (response) {
                         var data = [];
+                        var volData = [];
+                        vm.description = response.data.dataset.description;
                         vm.indexValue = response.data.dataset.data[0][1];
                         vm.high = response.data.dataset.data[0][2];
                         vm.low = response.data.dataset.data[0][3];
-                        vm.totalMarketValue = response.data.dataset.data[0][4];
+                        vm.totalMarketValue = "$"+response.data.dataset.data[0][5];
                         _.forEach(response.data.dataset.data, function (day) {
                             data.push([
                                 new Date(day[0]).getTime(),
@@ -28,23 +31,86 @@
                             ]);
                         })
                         _.reverse(data);
-                        $('#chartContainer').highcharts('StockChart', {
+                        $('#chartContainer1').highcharts('StockChart', {
                             rangeSelector: {
                                 selected: 1
                             },
                             title: {
-                                text: vm.Stock.code
+                                text: vm.code
                             },
                             series: [{
-                                name: vm.Stock.code,
+                                name: vm.code,
                                 data: data,
                                 tooltip: {
                                     valueDecimals: 2
                                 }
                             }]
                         });
+                        data = [];
+                        _.forEach(response.data.dataset.data, function (day) {
+                            data.push([
+                                new Date(day[0]).getTime(),
+                                day[1],
+                                day[2],
+                                day[3],
+                                day[4]
+                            ]);
+                            volData.push([
+                                new Date(day[0]).getTime(),
+                                day[5]
+                            ])
+                        });
+
+                        _.reverse(data);
+                        _.reverse(volData);
+
+                        $('#chartContainer2').highcharts('StockChart', {
+                            rangeSelector: {
+                                selected: 1
+                            },
+                            title: {
+                                text: vm.code
+                            },
+                            yAxis: [{
+                                labels: {
+                                    align: 'right',
+                                    x: -3
+                                },
+                                title: {
+                                    text: 'O H L C'
+                                },
+                                height: '60%',
+                                lineWidth: 2
+                            }, {
+                                labels: {
+                                    align: 'right',
+                                    x: -3
+                                },
+                                title: {
+                                    text: 'Volume'
+                                },
+                                top: '65%',
+                                height: '35%',
+                                offset: 0,
+                                lineWidth: 2
+                            }],
+                            series: [{
+                                type: 'candlestick',
+                                name: vm.code,
+                                data: data,
+                                tooltip: {
+                                    valueDecimals: 2
+                                }
+                            }, {
+                                type: 'column',
+                                name: 'Volume',
+                                data: volData,
+                                yAxis: 1
+                            }]
+                        });
+
                     }, function (err) {
-                        vm.Error = err.data;
+                        console.log("error");
                     });
             },function (err) {
                 vm.Error = err.data;
