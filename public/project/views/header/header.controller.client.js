d5b6252683b36d7caecbc899031666254756ebc7
PriyaArun <lakprigan@gmail.com>
2016-06-24 22:03:05 -0400

    sepearated the links and added links

---
+++ public/project/views/header/header.controller.client.js
@@ -0,0 +1,53 @@
+/**
+ * Created by PriyaArun on 6/24/16.
+ */
+(function () {
+    angular
+        .module("StockWatch")
+        .controller("HeaderController", HeaderController);
+
+    function HeaderController($location, UserService, $rootScope) {
+
+        var ViewModel = this;
+
+        ViewModel.Logout = Logout;
+        init();
+
+
+        $rootScope.$on("userChanged", function () {
+            init();
+        });
+
+
+
+        function init() {
+            UserService
+                .LoggedIn()
+                .then(function (resposnse) {
+                        var user = resposnse.data;
+                        if (user === '0') {
+                            ViewModel.currentUser = null;
+                            $location.url("/login");
+                        } else {
+                            ViewModel.currentUser = user;
+                        }
+                    },
+                    function (err) {
+                        $location.url("/login");
+                    });
+        }
+
+
+        function Logout() {
+            ViewModel.currentUser = null;
+            UserService
+                .Logout()
+                .then(function (res) {
+                        $location.url("/login");
+                    },
+                    function (err) {
+                        $location.url("/login");
+                    })
+        }
+    }
+})();
