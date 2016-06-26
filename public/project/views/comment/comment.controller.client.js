67cc1d191b6e681c71f6238c9fb6ae885fa5becd
PriyaArun <lakprigan@gmail.com>
2016-06-25 22:44:00 -0400

    refactored ui and formatted comments

--- public/project/views/comment/comment.controller.client.js
+++ public/project/views/comment/comment.controller.client.js
@@ -9,7 +9,7 @@
     function CommentController(CommentService, StockService, $routeParams, $rootScope) {
 
         var ViewModel = this;
-
+        ViewModel.SubmittedClass = "";
         ViewModel.currentUser = $rootScope.currentUser;
         ViewModel.UserId = $routeParams.id;
         ViewModel.PortfolioId = $routeParams.pid;
@@ -36,16 +36,22 @@
         }
 
         function CreateComment(comment) {
-            comment.username = ViewModel.currentUser.username;
-            comment.code = ViewModel.Stock.code;
-            comment.dateCreated = Date.now();
-            CommentService
-                .CreateComment(comment)
-                .then(function (res) {
-                    Initialize();
-                },function (err) {
-                    ViewModel.Error = "Unable to update the comment"
-                })
+            ViewModel.SubmittedClass = "submitted";
+            if (comment) {
+                comment.username = ViewModel.currentUser.username;
+                comment.code = ViewModel.Stock.code;
+                comment.dateCreated = Date.now();
+                CommentService
+                    .CreateComment(comment)
+                    .then(function (res) {
+                        Initialize();
+                    }, function (err) {
+                        ViewModel.Error = "Unable to post the comment"
+                    })
+            }
+            else{
+                ViewModel.Error = "Please enter the comment"
+            }
         }
     }
 
